6528df0596153905ca21d8ec00093ebc37ee4c4b
isantidev <santis.sierra6@gmail.com>
2025-09-19 12:36:40 -0500

    Color palette basic features added.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,25 @@
-import "@styles/global.css";
-import mineAI from "@images/mine-AI.jpeg";
+import { useState } from "react";
 
 const Header = () => {
+    const [theme, setTheme] = useState("light");
+
+    const toggletheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+
+        document.documentElement.classList.toggle("dark", newTheme === "dark");
+    };
+
     return (
         <>
-            <h1 className="font-onest text-5xl">Bienvenidos</h1>
-            <img src={mineAI} alt="foto mia" />
+            <div className="h-full flex items-center justify-center flex-col gap 4 transition-all ease-in-out ">
+                <button
+                    onClick={toggletheme}
+                    className="py-1.5 px-3 border rounded-full text-2xl cursor-pointer bg-custom-secondary text-custom-text"
+                >
+                    {theme === "light" ? ` Light mode ` : ` Dark mode `}
+                </button>
+            </div>
         </>
     );
 };
