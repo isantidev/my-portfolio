d82e1b933c1892e5bcbb8507d57c24702dfeb1cf
isantidev <santis.sierra6@gmail.com>
2025-09-20 20:38:57 -0500

    Hero component created, mobile layout added. Base color palette has been improved by adopting a more strong and vivid accent color

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -5,26 +5,30 @@ const Hero = () => {
         <>
             <section
                 aria-label="about-me"
-                className="max-w-screen-xl mx-auto my-4"
+                className="max-w-screen-xl mx-auto my-4 h-[80dvh] px-2"
             >
-                <div className="flex flex-col items-center justify-center gap-2">
-                    <span className="size-32 overflow-hidden rounded-full shadow-custom-text/60 shadow-[0px_0px_15px_0px] ">
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <span className="size-40 overflow-hidden rounded-full shadow-custom-accent/40 shadow-[0px_0px_15px_0px] ">
                         <img
                             className="h-full w-full object-cover"
                             src={profileImg}
                             alt=""
                         />
                     </span>
-                    <div className="flex flex-col justify-around items-center text-center">
+                    <div className="flex flex-col gap-2 justify-around items-center text-center">
                         <a
                             href="#"
-                            className="py-0.5 px-2 bg-custom-accent rounded-[0.25rem] max-w-fit opacity-90 hover:opacity-100 hover:contrast-150 focus:contrast-150"
+                            className="relative py-1 px-3 bg-transparent border border-custom-accent/60 rounded-[0.25rem] max-w-fit hover:animate-pulse transition-all"
                         >
-                            <span className="font-light text-custom-bg">
+                            <span class="absolute -top-1 -right-1 size-3 bg-custom-accent animate-ping rounded-full  opacity-60"></span>
+                            <span class="absolute -top-1 -right-1 size-3 rounded-full bg-custom-accent"></span>
+                            <span className="font-light text-custom-accent">
                                 open to work
                             </span>
                         </a>
-                        <h1 className="text-pretty">Kevin Sierra Rodriguez</h1>
+                        <h1 className="text-pretty text-">
+                            Kevin Sierra Rodriguez
+                        </h1>
                         <p className="text-balance">
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Nemo distinctio reiciendis officiis rem
