4748f6dd720625a1fbcd32d916ca3ad37104c1c1
isantidev <santis.sierra6@gmail.com>
2025-09-30 18:22:56 -0500

    Padding in every section is updated

---
+++ src/components/layout/Experience.jsx
@@ -0,0 +1,37 @@
+const Experience = () => {
+    return (
+        <>
+            <section
+                className="max-w-screen-xl w-full mx-auto px-4 min-h-screen mt-12 md:mt-16"
+                id="experience"
+            >
+                <h2>Experience</h2>
+                <div className="w-full flex flex-col gap-4">
+                    <div className="relative">
+                        <h4>Desktop Support IT</h4>
+
+                        <div className="flex gap-2 ">
+                            <span className="text-sm font-light capitalize">
+                                apprenticeship
+                            </span>
+                            <span className="text-sm font-light capitalize">
+                                Feb 2025 - Aug 2025
+                            </span>
+                        </div>
+                        <span>TP S.A.S.</span>
+                    </div>
+                    <ul>
+                        <li>
+                            Assisted in troubleshooting hardware and software
+                            issues
+                        </li>
+                        <li>Provided technical support to end-users</li>
+                        <li>Participated in IT projects and initiatives</li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Experience;
