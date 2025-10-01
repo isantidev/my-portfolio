48bf4e7441084f390244026dde96dff278614848
isantidev <santis.sierra6@gmail.com>
2025-10-01 13:04:36 -0500

    Navigation bar updated

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -30,17 +30,17 @@ const Navigation = () => {
 
     return (
         <>
-            <nav className="inline-block w-full min-h-[4.5rem] md:min-h-16 justify-center bg-custom-secondary z-999">
-                <div className="fixed top-0 right-0 left-0 w-full max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 h-auto bg-custom-secondary">
-                    <a href="#" className="flex items-center">
-                        <span className="self-center text-[1.2rem] font-semibold whitespace-nowrap">
+            <nav className="fixed md:top-2 w-screen min-h-fit inline-flex justify-center z-999">
+                <div className="w-full md:mx-4 md:rounded-sm max-w-screen-xl flex flex-wrap items-center justify-between p-4 h-auto backdrop-blur-3xl">
+                    <a href="#Hero" className="block">
+                        <span className="h-full align-middle text-xl font-bold">
                             isantidev
                         </span>
                     </a>
                     <div className="flex items-center gap-3 md:gap-0 md:order-2">
                         <button
                             onClick={toggleTheme}
-                            className="size-8 rounded-full cursor-pointer bg-custom-primary/40 hover:bg-custom-accent/20 hover:shadow-sm hover:shadow-custom-text/20"
+                            className="size-8 rounded-sm cursor-pointer bg-custom-primary/40 hover:bg-custom-accent/20 hover:shadow-sm hover:shadow-custom-text/20"
                         >
                             {theme === "light" ? (
                                 <IoMdSunny className="size-4 m-auto" />
@@ -52,15 +52,15 @@ const Navigation = () => {
                             onClick={toggleNavbar}
                             data-collapse-toggle="navbar-solid-bg"
                             type="button"
-                            className="inline-flex items-center p-2 size-10 justify-center text-sm text-custom-text rounded-lg md:hidden"
+                            className="inline-flex items-center size-8 justify-center text-sm text-custom-text rounded-sm md:hidden"
                             aria-controls="navbar-solid-bg"
                             aria-expanded={isExpanded}
                         >
                             <span className="sr-only">Open main menu</span>
                             {isExpanded === true ? (
-                                <IoClose className="size-6 m-auto stroke-3" />
+                                <IoClose className="size-7 stroke-3" />
                             ) : (
-                                <IoMenu className="size-6 m-auto stroke-3" />
+                                <IoMenu className="size-7 stroke-3" />
                             )}
                         </button>
                     </div>
@@ -73,32 +73,32 @@ const Navigation = () => {
                         <ul className="flex flex-col font-medium mt-4 rounded-lg md:rounded-none overflow-hidden bg-custom-primary/40 md:gap-4 md:flex-row md:mt-0 md:border-0 md:bg-transparent">
                             <li>
                                 <a
-                                    href="#"
-                                    className="block py-2 px-3 md:py-0.5 text-custom-text/90 md:rounded-sm md:overflow-auto hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
+                                    href="#Experience"
+                                    className="block py-2 px-3 md:py-1 text-custom-text/90 md:rounded-sm md:overflow-auto hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
                                 >
                                     Experience
                                 </a>
                             </li>
                             <li>
                                 <a
-                                    href="#"
-                                    className="block py-2 px-3 md:py-0.5 text-custom-text/90 md:rounded-sm hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
+                                    href="#Projects"
+                                    className="block py-2 px-3 md:py-1 text-custom-text/90 md:rounded-sm hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
                                 >
                                     Projects
                                 </a>
                             </li>
                             <li>
                                 <a
-                                    href="#"
-                                    className="block py-2 px-3 md:py-0.5 text-custom-text/90 md:rounded-sm hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
+                                    href="#About"
+                                    className="block py-2 px-3 md:py-1 text-custom-text/90 md:rounded-sm hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
                                 >
                                     About me
                                 </a>
                             </li>
                             <li>
                                 <a
-                                    href="#"
-                                    className="block py-2 px-3 md:py-0.5 text-custom-text/90 md:rounded-sm hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
+                                    href="#Contact"
+                                    className="block py-2 px-3 md:py-1 text-custom-text/90 md:rounded-sm hover:text-custom-text focus:bg-custom-accent/20 md:border-0 md:hover:bg-custom-accent/20 "
                                 >
                                     Contact
                                 </a>
