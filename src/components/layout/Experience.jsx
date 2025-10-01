7810b291ee157e980ba435a7b40a2fb27ca7f44f
isantidev <santis.sierra6@gmail.com>
2025-10-01 18:58:52 -0500

    Refactor Experience component layout and update color variables in global styles

--- src/components/layout/Experience.jsx
+++ src/components/layout/Experience.jsx
@@ -2,25 +2,34 @@ const Experience = () => {
     return (
         <>
             <section
-                className="scroll-m-20 w-full mx-auto md:max-w-2xl lg:max-w-screen-xl px-4"
+                className="scroll-m-20 w-full mx-auto flex flex-col gap-2 lg:max-w-screen-xl px-4 pb-20
+                md:max-w-2xl"
                 id="Experience"
             >
                 <h2>Experience</h2>
-                <div className="w-full flex flex-col gap-4">
-                    <div className="relative">
-                        <h4>Desktop Support IT</h4>
 
-                        <div className="flex gap-2 ">
-                            <span className="text-sm font-light capitalize">
-                                apprenticeship
-                            </span>
-                            <span className="text-sm font-light capitalize">
-                                Feb 2025 - Aug 2025
-                            </span>
-                        </div>
-                        <span>TP S.A.S.</span>
+                <article
+                    tabIndex={0}
+                    className="grid gap-4 p-2 rounded-sm hover:bg-custom-secondary focus:bg-custom-secondary transition ease-in-out
+                    md:p-4 lg:py-8 lg:px-14 md:grid-cols-2"
+                >
+                    <div className="relative flex flex-col">
+                        <h4 className="text-custom-primary">
+                            Desktop Support IT
+                        </h4>
+
+                        <span>
+                            <strong>TP S.A.S.</strong>
+                        </span>
+                        <span className="text-md font-light capitalize">
+                            Feb 2025 - Aug 2025
+                        </span>
                     </div>
-                    <ul>
+                    <ul
+                        className="*:text-lg list-disc pl-6
+                        md:pl-8"
+                    >
+                        <li></li>
                         <li>
                             Assisted in troubleshooting hardware and software
                             issues
@@ -28,7 +37,7 @@ const Experience = () => {
                         <li>Provided technical support to end-users</li>
                         <li>Participated in IT projects and initiatives</li>
                     </ul>
-                </div>
+                </article>
             </section>
         </>
     );
