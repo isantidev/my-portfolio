60038b19c7d6aa6bf28ff26666cd6fc10c3b9816
isantidev <santis.sierra6@gmail.com>
2025-10-01 16:43:13 -0500

    Font issue fixed, now Onest Variable is applied to the system

--- src/components/layout/Hero.jsx
+++ src/components/layout/Hero.jsx
@@ -6,7 +6,8 @@ const Hero = () => {
         <>
             <section
                 aria-label="about-me"
-                className="py-28 md:h-auto md:py-36 scroll-m-20 w-full mx-auto lg:max-w-screen-xl md:max-w-2xl px-4"
+                className="py-24 md:h-auto md:py-36 scroll-m-20 w-full mx-auto lg:max-w-screen-xl md:max-w-2xl px-4"
+                id="Hero"
             >
                 <div className="flex flex-col items-center justify-between gap-4 md:grid md:grid-cols-3 md:place-items-center md:gap-8">
                     <div className="flex flex-col h-full items-center justify-around gap-4 md:gap-2">
@@ -26,23 +27,23 @@ const Hero = () => {
                         >
                             <span className="absolute -top-1 -right-1 size-3 bg-custom-primary animate-ping rounded-full opacity-80"></span>
                             <span className="absolute -top-1 -right-1 size-3 rounded-full bg-custom-primary"></span>
-                            <span className="text-custom-accent/80">
+                            <span className="text-custom-accent/80 text-lg">
                                 Available to work!
                             </span>
                         </a>
                     </div>
 
-                    <div className="flex flex-col max-w-xl h-full gap-2 justify-between items-center text-center md:col-span-2 md:items-start">
+                    <article className="flex flex-col max-w-xl h-full gap-2 justify-between items-center text-center md:col-span-2 md:items-start">
                         <h1 className="text-pretty md:text-left">
                             Hey, I'm Kevin Sierra
                         </h1>
-                        <p className="text-balance md:text-left max-w-md">
+                        <p className="text-balance text-xl md:text-left max-w-md">
                             <strong className="text-custom-accent">
                                 {"Software Developer "}
                             </strong>
                             focused on building reliable, high-quality
                             applications with
-                            <strong className="text-[#61DBFB]">
+                            <strong className="text-[#60dafb]">
                                 {" React.js"}
                             </strong>
                             . Skilled in Agile collaboration and effective in
@@ -57,7 +58,7 @@ const Hero = () => {
                                 </i>
                             </small>
                         </p>
-                    </div>
+                    </article>
                 </div>
             </section>
         </>
