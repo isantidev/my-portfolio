2d90ba391b66a4953375b5968e50a68257baaa3d
isantidev <santis.sierra6@gmail.com>
2025-10-02 18:34:08 -0500

    Imports in main.jsx updated, using the new alias for the layout components

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Navigation from "./components/layout/Navigation.jsx";
-import Hero from "./components/layout/Hero.jsx";
-import Experience from "./components/layout/Experience.jsx";
+import Navigation from "@layout/Navigation.jsx";
+import Hero from "@layout/Hero.jsx";
+import Experience from "@layout/Experience.jsx";
 import "@styles/global.css";
 import "@fontsource-variable/onest";
 
