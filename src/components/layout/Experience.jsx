1a535662b82e60c4e7a14af0a1f11630694f7c98
Kevin Sierra Rodriguez <santis.sierra6@gmail.com>
2025-10-01 22:56:09 -0500

    C/exp 19 (#28)
    * Refactor Experience component layout and update color variables in global styles
    
    * Experience component fulfilled
    
    * Refactor Experience component layout and adjust spacing in global styles

--- src/components/layout/Experience.jsx
+++ src/components/layout/Experience.jsx
@@ -1,34 +1,74 @@
+import { useState } from "react";
+import { IoBriefcaseSharp } from "react-icons/io5";
+
 const Experience = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const showMoreInfo = () => {
+        setIsExpanded(!isExpanded);
+    };
+
     return (
         <>
             <section
-                className="scroll-m-20 w-full mx-auto md:max-w-2xl lg:max-w-screen-xl px-4"
+                className="scroll-m-20 w-full mx-auto flex flex-col gap-4 lg:max-w-screen-xl px-4 pb-20
+                md:max-w-2xl"
                 id="Experience"
             >
-                <h2>Experience</h2>
-                <div className="w-full flex flex-col gap-4">
-                    <div className="relative">
-                        <h4>Desktop Support IT</h4>
+                <header className="inline-flex gap-2 align-middle">
+                    <span className="size-8 place-content-center p-1 *:mx-auto *:size-full">
+                        <IoBriefcaseSharp />
+                    </span>
+                    <h2>Experience</h2>
+                </header>
 
-                        <div className="flex gap-2 ">
-                            <span className="text-sm font-light capitalize">
-                                apprenticeship
-                            </span>
-                            <span className="text-sm font-light capitalize">
-                                Feb 2025 - Aug 2025
-                            </span>
-                        </div>
-                        <span>TP S.A.S.</span>
+                <article
+                    tabIndex={0}
+                    className="grid gap-4 p-2 rounded-sm hover:bg-custom-secondary focus:bg-custom-secondary transition ease-in-out
+                    md:p-4 lg:py-8 lg:px-14 md:grid-cols-2"
+                >
+                    <div className="relative flex flex-col gap-1">
+                        <h4 className="text-custom-primary">
+                            Desktop Support IT
+                        </h4>
+
+                        <span>
+                            <strong>TP S.A.S.</strong>
+                        </span>
+                        <span className="text-md font-light capitalize">
+                            Feb 2025 - Aug 2025
+                        </span>
                     </div>
-                    <ul>
-                        <li>
-                            Assisted in troubleshooting hardware and software
-                            issues
-                        </li>
-                        <li>Provided technical support to end-users</li>
-                        <li>Participated in IT projects and initiatives</li>
-                    </ul>
-                </div>
+                    <p className="text-lg">
+                        Developed and optimized a custom Linux bootable system,
+                        achieving a 40% reduction in ISO size (2.7 GB to 1.6 GB)
+                        and 37% faster boot times through terminal-based
+                        configuration.{" "}
+                        <span className="sr-only">Show more info</span>
+                        <span
+                            className={isExpanded === true ? "block" : "hidden"}
+                        >
+                            {" "}
+                            Performed system hardening, ensured cross-platform
+                            compatibility with Ventoy and Rufus, and created
+                            comprehensive technical documentation. Delivered
+                            technical support and troubleshooting while
+                            contributing to IT projects and system improvements.
+                        </span>
+                        <button
+                            className={
+                                isExpanded === false
+                                    ? "block font-light text-custom-primary/80 mt-1"
+                                    : "block font-light text-custom-primary/80 mt-1"
+                            }
+                            onClick={showMoreInfo}
+                        >
+                            {isExpanded === true
+                                ? "< Read less"
+                                : "Read more >"}
+                        </button>
+                    </p>
+                </article>
             </section>
         </>
     );
