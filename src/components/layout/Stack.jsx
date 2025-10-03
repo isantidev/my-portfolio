4fe0abf73291274897dac6fe3a6c234e0dfc6ac8
isantidev <santis.sierra6@gmail.com>
2025-10-02 19:02:20 -0500

    Add Stack component and integrate it into main rendering

---
+++ src/components/layout/Stack.jsx
@@ -0,0 +1,11 @@
+const Stack = () => {
+    return (
+        <>
+            <section className="min-w-dvh min-h-24 h-12 bg-custom-secondary mb-20">
+                <ul className="h-full"></ul>
+            </section>
+        </>
+    );
+};
+
+export default Stack;
