23b2d611b574fd3d3a917dcf1042163538a73e6c
isantidev <santis.sierra6@gmail.com>
2025-10-10 13:06:38 -0500

    Quick fix

--- src/components/ui/Pill.jsx
+++ src/components/ui/Pill.jsx
@@ -3,7 +3,7 @@ const Pill = ({ key, text }) => {
         <>
             <li
                 key={key}
-                className="py-1.5 px-3 text-md bg-custom-accent/20 rounded-md capitalize"
+                className="py-2 px-4 text-md bg-custom-accent/20 rounded-md capitalize"
             >
                 {text}
             </li>
