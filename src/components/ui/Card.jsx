0b564cae9a6cdfc936cec9f17885375229007772
isantidev <santis.sierra6@gmail.com>
2025-10-10 12:36:07 -0500

    The card for a project has been created

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -25,7 +25,7 @@ export const CardJob = ({ title, enterprise, time, description }) => {
                     md:p-4 lg:py-8 lg:px-14 md:grid-cols-2"
             >
                 <div className="relative flex flex-col gap-1">
-                    <h4 className="text-custom-primary">{title}</h4>
+                    <h3 className="text-custom-primary">{title}</h3>
 
                     <span>
                         <strong>{enterprise}</strong>
@@ -55,18 +55,38 @@ export const CardJob = ({ title, enterprise, time, description }) => {
 export const CardProject = (project) => {
     return (
         <>
-            <article>
-                <figure>
+            <article className="w-full py-6 px-8 rounded-sm shadow-custom-primary/20 shadow-[0px_0px_15px_0px] flex flex-col gap-2">
+                <figure className="w-full rounded-sm flex flex-col justify-between">
                     <img
                         src={project.image.src}
                         alt={project.image.description}
+                        loading="lazy"
+                        className="p-2 aspect-[4/3] max-h-60 rounded-t-sm object-cover overflow-hidden bg-custom-secondary/40"
                     />
-                    <span>{project.image.description}</span>
+                    <span
+                        aria-label="imageCaption"
+                        className="block text-custom-text/60 font-light text-sm text-center py-3 px-auto"
+                    >
+                        {project.image.description}
+                    </span>
                 </figure>
-                <h4>{project.title}</h4>
-                <ul>{project.stack}</ul>
-                <p>{project.description}</p>
-                <button>{project.status}</button>
+                <h3 className="text-custom-primary leading-5">
+                    {project.title}
+                </h3>
+                <p className="leading-6 font-light text-md">
+                    {project.description}
+                </p>
+                <div className="w-full h-fit">
+                    <h5 className="text-custom-text/90">Technology Stack</h5>
+                    <ul className="flex flex-wrap gap-4 gap-y-1">
+                        {project.stack.map((item) => {
+                            return <li key={item}>{item}</li>;
+                        })}
+                    </ul>
+                </div>
+                <button className="p-2 max-w-2/3 mt-4 bg-custom-primary/40 rounded-sm capitalize">
+                    {project.status === "Live" ? "View project" : "Preview"}
+                </button>
             </article>
         </>
     );
