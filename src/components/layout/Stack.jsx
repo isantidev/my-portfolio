49927404f6e5c50c49fc046424544320f10c405f
isantidev <santis.sierra6@gmail.com>
2025-11-04 12:17:12 -0500

    fix imports

--- src/components/layout/Stack.jsx
+++ src/components/layout/Stack.jsx
@@ -1,4 +1,4 @@
-import sprite from "@icons/sprite.svg";
+import { IoCodeSlash } from "react-icons/io5";
 
 const Stack = () => {
     return (
@@ -9,49 +9,71 @@ const Stack = () => {
                 md:max-w-2xl"
                 id="Stack"
             >
+                <header className="inline-flex gap-2 align-middle">
+                    <span className="w-8 h-full p-1 *:w-full flex justify-center">
+                        <IoCodeSlash className="my-auto size-full" />
+                    </span>
+                    <h2>Stack</h2>
+                </header>
+                <p className="ml-4 text-xl text-custom-text/80">
+                    Tools and technologies I use to build websites.
+                </p>
+
                 <ul
-                    className="grid grid-cols-2 place-items-center
-                    [&_svg]:size-20 [&_svg]:p-4"
+                    className="grid grid-cols-5 place-items-center
+                    [&_svg]:size-24 [&_svg]:p-4 "
                 >
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#javascript`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">
+                            Javascript
+                        </small>
                     </li>
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#tailwindcss`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">
+                            Tailwind CSS
+                        </small>
                     </li>
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#git`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">Git</small>
                     </li>
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#react`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">React JS</small>
                     </li>
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#html5`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">HTML5</small>
                     </li>
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#css`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">CSS3</small>
                     </li>
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#mysql`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">MySQL</small>
                     </li>
-                    <li tabIndex={0} className="">
+                    <li className="flex flex-col justify-center items-center">
                         <svg>
-                            <use href={`${sprite}#vscode`}></use>
+                            <use href={``}></use>
                         </svg>
+                        <small className="font-medium text-lg">VS Code</small>
                     </li>
                 </ul>
             </section>
