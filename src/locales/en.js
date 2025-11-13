ad2d9755949d37dbecfc578165738cb03660fb58
isantidev <santis.sierra6@gmail.com>
2025-11-13 14:36:07 -0500

    feat: migrate nav content to manage lanaguage toggle

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,13 +1,8 @@
 export default {
     nav: {
-        home: "Home",
         experience: "Experience",
         projects: "Projects",
         about: "About me",
         contact: "Contact",
-    },
-    hero: {
-        title: "Hey, I'm Santiago",
-        content: {},
     },
 };
