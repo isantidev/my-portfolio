d6365394bd5be8159b6f7aa089343cdef9e38c9a
isantidev <santis.sierra6@gmail.com>
2025-11-13 15:21:09 -0500

    feat: migrate Experience content to manage language toggle

--- src/locales/en.js
+++ src/locales/en.js
@@ -24,4 +24,16 @@ export default {
         },
         callToAction: "Available to work!",
     },
+    experience: {
+        title: "Experience",
+        content: {
+            tp: {
+                title: "Desktop & Remote Support IT",
+                enterprise: "TP S.A.S.",
+                time: "Feb 2025 - Aug 2025",
+                description:
+                    "Developed and optimized a custom Linux bootable system, achieving a 40% reduction in ISO size (2.7 GB to 1.6 GB) and 37% faster boot times through terminal-based configuration. Performed system hardening, delivered technical support, level 1 and 2. Troubleshooting while contributing to IT projects and system improvements; creating comprehensive technical documentation.",
+            },
+        },
+    },
 };
--- src/locales/es.js
+++ src/locales/es.js
@@ -24,4 +24,16 @@ export default {
         },
         callToAction: "¡Disponible para trabajar!",
     },
+    experience: {
+        title: "Experiencia",
+        content: {
+            tp: {
+                title: "Soporte IT - Sitio & Remoto",
+                enterprise: "TP S.A.S.",
+                time: "Feb 2025 - Ago 2025",
+                description:
+                    "Desarrollé y optimicé un sistema Linux, reduciendo en un 40% el tamaño del archivo ISO (2.7 GB a 1.6 GB) y mejorando la velocidad de booteo en un 37%. Mis labores eran la validación del hardening de computadores, realizar labores de soporte, nivel 1 y 2, manejo de errores, al igual que contribuir en proyectos que se me fuera solicitado; creando documentación técnica.",
+            },
+        },
+    },
 };
